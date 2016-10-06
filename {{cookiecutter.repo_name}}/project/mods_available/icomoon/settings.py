6fc1bd7087bfa24f38f608ec16f5f3f0c7c55267
David THENON <sveetch@gmail.com>
2016-10-06 14:02:33 +0200

    Fixed icomoon mod settings for 'sass' dir new path, close #69

--- {{cookiecutter.repo_name}}/project/mods_available/icomoon/settings.py
+++ {{cookiecutter.repo_name}}/project/mods_available/icomoon/settings.py
@@ -6,6 +6,6 @@ INSTALLED_APPS = add_to_tuple(INSTALLED_APPS, 'icomoon')
 ICOMOON_WEBFONTS = {
     'Default': {
         'fontdir_path': join(PROJECT_PATH, 'webapp_statics/fonts'),
-        'csspart_path': join(PROJECT_PATH, '..', "compass/scss/components/_icomoon_icons.scss"),
+        'csspart_path': join(PROJECT_PATH, '..', "sass/scss/components/_icomoon_icons.scss"),
     },
 }
